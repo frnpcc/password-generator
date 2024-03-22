6d2467f4eb859422700c020ab9e81ec62d1b5b97
Francesca Correa <francescacciavaglia@gmail.com>
2024-03-22 20:45:23 +0100

    change let to const and comment out console log

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 
-let generateButton = document.querySelector("#generate-btn")
-let passwordElOne = document.querySelector("#password-one")
-let passwordElTwo = document.querySelector("#password-two")
+const generateButton = document.querySelector("#generate-btn")
+const passwordElOne = document.querySelector("#password-one")
+const passwordElTwo = document.querySelector("#password-two")
 
 generateButton.addEventListener("click", function () {
-  console.log("clicked")
-  let passwordOne = generatePassword()
-  let passwordTwo = generatePassword()
+  // console.log("clicked")
+  const passwordOne = generatePassword()
+  const passwordTwo = generatePassword()
   passwordElOne.textContent = passwordOne
   passwordElTwo.textContent = passwordTwo
 } )
